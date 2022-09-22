97b8a99e555c0f1f041911c4466c9ce86bfeadd0
Gideon Oba <deyonoba@gmail.com>
2022-09-22 07:05:19 -0700

    feat: add new feature "calculate_mean"

--- gaussian.py
+++ gaussian.py
@@ -6,15 +6,25 @@ class Gaussian():
     Create Gaussian class for calculation and visualization of a Gaussian distribution.
 
     Attributes
-    ----------
-    mean : float
-        The mean value of the distribution data.
-    stdev : float
-        The standard deviation of the distribution data.
-    data : list of float
-        List of float numbers extracted from the data file.
+        mean : float
+            The mean value of the distribution data.
+        std : float
+            The standard deviation of the distribution data.
+        data : list of float
+            List of float numbers extracted from the data file.
     """
+
     def __init__(self, mu=0, sigma=1):
         self.mean = mu
-        self.stdev = sigma
+        self.std = sigma
         self.data = []
+
+    def calculate_mean(self):
+        """Function to calculate the mean of the data set.
+
+        Returns:
+            float: mean of the data set
+        """
+        avg = 1 * sum(self.data) / len(self.data)
+        self.mean = avg
+        return self.mean
